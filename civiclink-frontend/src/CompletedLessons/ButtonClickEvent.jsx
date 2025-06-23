8c3e088f2dcfa2ad80227f18d1e0b7777c53f540
vinod <vinodgunaganti29@gmail.com>
2025-06-23 18:16:58 +0530

    structured the whole project by creating the components and pages anddirectory.

--- civiclink-frontend/src/CompletedLessons/ButtonClickEvent.jsx
+++
@@ -1,7 +0,0 @@
-function ButtonClickEvent(){
-
-    const handleClick = (e) => e.target.textContent = "Dead";
-
-     return(<button onDoubleClick={(e) => handleClick(e)}>HouseFly</button>)
-}
-export default ButtonClickEvent
